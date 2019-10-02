d7864182bbeaab0db9699e347176a4b36513a492
Obinna Igwe <igwenus619@gmail.com>
2019-10-02 13:46:01 +0100

    Saved work and served locally. If you do not save your code and you serve/deploy, you will not get a cloud functions url

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,11 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+
+    const name = request.query.name;
+
+    response.send(`hello ${name}`);
+});
