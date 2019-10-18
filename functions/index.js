e1fbedef11f8b676f2c13d080b8b309f3ca0f4e8
Obinna Igwe <igwenus619@gmail.com>
2019-10-18 15:43:26 +0100

    Use express app to define endpoints

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require('firebase-functions');
 
+const express = require('express');
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -8,9 +10,22 @@ const functions = require('firebase-functions');
 // });
 
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
+// // We are exporting a function to be run on the cloud
+// // But we could use express.js to define a single app with 
+// //multiple routes (endpoint)
+// exports.helloWorld = functions.https.onRequest((request, response) => {
 
-    const name = request.query.name;
+//     const name = request.query.name;
 
-    response.send(`hello ${name}`);
+//     response.send(`hello ${name}`);
+// });
+
+
+const app = express();
+
+app.get('hello',(request, response) => {
+response.send('cat');
 });
+
+
+exports.api = functions.https.onRequest(app);
